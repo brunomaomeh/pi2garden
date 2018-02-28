b2f8adbee9b7165f7c0ba2b3e01a8cede1563dbc
Bruno Maomeh <brunomaomeh@gmail.com>
2018-02-27 22:46:46 -0300

    adiciona johnny five

--- controller/Garden.js
+++ controller/Garden.js
@@ -1,10 +1,25 @@
+var five = require("johnny-five");
+var Raspi = require("raspi-io");
+
+var board = new five.Board({
+	  io: new Raspi()
+});
+
+board.on("ready", function() {
+	console.log("Ligou!");
+});
+
 class Garden {
   on() {
+    var led = new five.Led("P1-13");
+    led.write(1);
     console.log('on');
   }
 
   off() {
-    console.log('off');
+    var led = new five.Led("P1-13");
+    led.write(0);
+    console.log("Iniciou!");
   }
 }
 
